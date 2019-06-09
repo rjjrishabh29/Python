73e13868a1c3036218ad40547d19362ed6ea3785
Rishabh <2017pcecsrishabh131@poornima.org>
2019-06-09 22:32:53 +0530

    Add files via upload

---
+++ age_95.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+print("ENTER YOUR NAME ")
+name = input()
+print("ENTER YOUR AGE ")
+age = input()
+age_at95 = ((datetime.now().year)+95-(int(age)))
+print("Age at 95 will be : " , age_at95)
+
---
+++ google_search.py
@@ -0,0 +1,9 @@
+#!/bin/Python3
+import webbrowser
+from googlesearch import search
+f=open("glinkss",'w')
+web =input("ENTER TOPIC TO SEARCH ON GOOGLE :")
+topic=search(web,stop=10)
+for i in topic:
+	f.write(i + "\n")
+webbrowser.open("https://www.google.com/search?q="+topic)
