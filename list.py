e86ad98040c672eaa29aad7e124658a277881787
Rishabh <2017pcecsrishabh131@poornima.org>
2019-06-10 16:48:04 +0000

    Create list.py

---
+++ list.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+num_lar_than5=[i for i in adhoc if i>5]
+print("NUMBER GREATER THAN 5",num_lar_than5)
+num_smae_than2=[i for i in adhoc if i<=2]
+print("NUMBER SMALLER THAN 2", num_smae_than2)
