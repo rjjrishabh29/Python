af0e689a4f9ecd07c17133285fc2b915435e1bec
Rishabh <2017pcecsrishabh131@poornima.org>
2019-06-11 22:42:41 +0530

    Update greetaspertime.py

--- greetaspertime.py
+++ greetaspertime.py
@@ -3,11 +3,11 @@ from datetime import datetime
 name=input()
 hour=int((datetime.now()).strftime("%H"))
 if hour  <=12 :
-        print("GOOD MORNING",name)
+        print("GOOD  MORNING",name)
 elif hour  <=18 :
-        print("GOOD AFTERNOON",name)
+        print("GOOD  AFTERNOON",name)
 elif hour  <=22 :
-        print("GOOD EVENING",name)
+        print("GOOD  EVENING",name)
 elif hour  <=24 :
-        print("GOOD NIGHT",name)
+        print("GOOD  NIGHT",name)
 
