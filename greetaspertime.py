02772bd8857f8bc848163691dc9556464085c6ea
Rishabh <2017pcecsrishabh131@poornima.org>
2019-06-15 16:20:47 +0530

    Probelm 5
    Write a code  will take  input as your name and greet you with
    good morning , good evening , goodafter noon , good night as per the current time your system .

--- greetaspertime.py
+++ greetaspertime.py
@@ -5,7 +5,7 @@ hour=int((datetime.now()).strftime("%H"))
 if hour  <=12 :
         print("GOOD  MORNING",name)
 elif hour  <=18 :
-        print("GOOD  AFTERNOON",name)
+        print("GOOD  AFTERNOON",name) 
 elif hour  <=22 :
         print("GOOD  EVENING",name)
 elif hour  <=24 :
