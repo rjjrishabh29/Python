7403e7fbef85221a5dd003d79917e568b9f1dbd5
Rishabh <2017pcecsrishabh131@poornima.org>
2019-06-15 16:13:22 +0530

    Update list.py
    Take a list say  adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
    write the program that will do
      i)  print only those numbers greater than 5
      ii)  also print those numbers those are less than or  equals to 2  ( <= 2 )
      iii)  store these answers in in two different list also

--- list.py
+++ list.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+adhoc=[1,2,3,1,4,5,66,22,2,6,0,9] 
 num_lar_than5=[i for i in adhoc if i>5]
 print("NUMBER GREATER THAN 5",num_lar_than5)
 num_smae_than2=[i for i in adhoc if i<=2]
